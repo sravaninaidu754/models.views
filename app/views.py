c4251e2453fcea18716ca348b19bf311fa405f27
sravaninaidu754 <sravaninaidu754@gmail.com>
2023-06-21 21:58:26 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from app.models import *
+from django.http import HttpResponse
+# Create your views here.
+def insert_TOpic(request):
+    TN=input('Enter your Topicname : ')
+    TO=Topic.objects.get_or_create(topic_name=TN)[0]
+    TO.save()
+
+def insert_Webpage(request):
+    TN=input('Enter your Topicname : ')
+    TO=Topic.objects.get_or_create(topic_name=TN)[0]
+    TO.save()
+    # return HttpResponse("Topic data inserted successfully")
+    n=input('Enter your name : ')
+    u=input('enter your url : ')
+    WO=Webpage.objects.get_or_create(topic_name=TO,n=name,url=u)[0]
+    WO.save()
+    return HttpResponse("Webpage data inserted successfully")
+
+def insert_Accessrecords(request):
+    TN=input('Enter your topic name : ')
+    TO=Topic.objects.get_or_create(topic_name=TN)[0]
+    TO.save()
+    n=input('enter your name : ')
+    u=input('enter your url : ')
+    WO=Webpage.objects.get_or_create(topic_name=TO,name=n,url=u)[0]
+    WO.save()
+    d=input('Enter date : ')
+    A=input('enter author name : ')
+    AR=Accessrecords.objects.get_or_create(name=WO,date=d,Authour=A)[0]
+    AR.save()
+    return HttpResponse("Aceessrecords data inserted successfully")
